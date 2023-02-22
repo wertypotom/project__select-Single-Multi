0dbc2d99c400b1657fe2b9b2e73c30c42b1a1818
Andrey Povstyanko <werty.potom@gmail.com>
2023-02-22 21:42:40 +0600

    added multiselect features

--- src/App.tsx
+++ src/App.tsx
@@ -19,17 +19,31 @@ const options: SelectOptions[] = [
         value: 4,
         label: "Fourth",
     },
+    {
+        value: 5,
+        label: "Fifth",
+    },
 ];
 
 function App() {
     const [value, setValue] = useState<SelectOptions | undefined>(options[0]);
+    const [values, setValues] = useState<SelectOptions[]>([]);
 
     return (
-        <Select
-            value={value}
-            options={options}
-            onChange={(option) => setValue(option)}
-        />
+        <>
+            <Select
+                value={value}
+                options={options}
+                onChange={(option) => setValue(option)}
+            />
+            <br></br>
+            <Select
+                multiple
+                value={values}
+                options={options}
+                onChange={(option) => setValues(option)}
+            />
+        </>
     );
 }
 
--- src/UI/Select/Select.tsx
+++ src/UI/Select/Select.tsx
@@ -1,29 +1,110 @@
-import React from "react";
+import { useState, MouseEvent, useEffect } from "react";
 import { SelectOptions } from "../../types/TSelect";
 import style from "./Select.module.css";
 
-type Props = {
+type SingleSelectProps = {
+    multiple?: false;
     onChange: (value: SelectOptions | undefined) => void;
-    options: SelectOptions[];
     value?: SelectOptions;
 };
 
-const Select = ({ onChange, options, value }: Props) => {
+type MultiSelectProps = {
+    multiple: true;
+    onChange: (value: SelectOptions[]) => void;
+    value: SelectOptions[];
+};
+
+type Props = {
+    options: SelectOptions[];
+} & (MultiSelectProps | SingleSelectProps);
+
+const Select = ({ multiple, onChange, options, value }: Props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [hoverIndex, setHoverIndex] = useState(0);
+
+    useEffect(() => {
+        if (!isOpen) setHoverIndex(0);
+    }, [isOpen]);
+
+    const clearOptions = (e: MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();
+
+        multiple ? onChange([]) : onChange(undefined);
+    };
+
+    const selectOption = (
+        e: MouseEvent<HTMLElement>,
+        option: SelectOptions
+    ) => {
+        e.stopPropagation();
+
+        if (multiple) {
+            if (value.includes(option)) {
+                // if value already selected
+                // remove value once clicked on it second time
+                onChange(value.filter((v) => v !== option));
+            } else {
+                // add new value to array of values
+                onChange([...value, option]);
+            }
+        } else {
+            if (option !== value) {
+                onChange(option);
+                setIsOpen(false);
+            }
+        }
+    };
+
+    const isOptionSelected = (option: SelectOptions) => {
+        // for multiples we highlight all chosen elements. For single only that currently selected
+        return multiple ? value.includes(option) : option === value;
+    };
+
+    const getSelectedOptions = () => {
+        return multiple
+            ? value.map((v) => (
+                  <button
+                      onClick={(event) => selectOption(event, v)}
+                      key={v.value}
+                      className={style.optionBadge}
+                  >
+                      {v.label}
+                      <span className={style.closeBtn}>&times;</span>
+                  </button>
+              ))
+            : value?.label;
+    };
+
     return (
-        <div tabIndex={0} className={style.container}>
+        <div
+            onBlur={() => setIsOpen(false)}
+            onClick={() => setIsOpen(!isOpen)}
+            tabIndex={0}
+            className={style.container}
+        >
             {/* tabIndex required to make a div focused. Focus styles are in a stylesheet */}
-            <span className={style.value}>{value?.label}</span>
-            <button className={style.closeBtn}>&times;</button>
+            <span className={style.value}>{getSelectedOptions()}</span>
+            <button onClick={clearOptions} className={style.closeBtn}>
+                &times;
+            </button>
             <div className={style.devider}></div>
             <div className={style.caret}></div>
-
-            <ul className={`${style.options}`}>
-                {options.map((option) => (
-                    <li className={style.option} key={option.label}>
-                        {option.label}
-                    </li>
-                ))}
-            </ul>
+            {isOpen && (
+                <ul className={`${style.options}`}>
+                    {options.map((option, index) => (
+                        <li
+                            onMouseEnter={() => setHoverIndex(index)}
+                            onClick={(event) => selectOption(event, option)}
+                            className={`${style.option} ${
+                                isOptionSelected(option) ? style.selected : ""
+                            } ${hoverIndex === index ? style.hovered : ""}`}
+                            key={option.label}
+                        >
+                            {option.label}
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
